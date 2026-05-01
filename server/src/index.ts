1c89d2f0bbf3d10ae6a22758d3104132d778ee3a
anstrcloud-dev <annacloudwork@gmail.com>
2026-05-01 20:31:19 +0200

    backend: seed logic and deezer api call

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,42 @@
 
 
 import express, { Request, Response } from 'express'
+import axios from 'axios'
+
 
 const app = express() // creates actual server instance
 const port = 3001
 
-app.get('/api/track', (req: Request, res: Response) => {
-  res.json({ message: "hello" })
+
+
+//builds number out of string
+const hashSeed = (str: string): number => {
+  let hash = 0
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash * 31 + str.charCodeAt(i)) % 1000000
+  }
+  return hash
+}
+
+const GENRES = ['pop', 'rock', 'jazz', 'electronic', 'classical', 'hip-hop', 'soul', 'indie']
+
+//route handler
+app.get('/api/track', async (req: Request, res: Response) => {
+  const userId = req.query.userId as string
+  const date = new Date().toISOString().slice(0, 10)
+  const seed = hashSeed(date + userId)
+  const genre = GENRES[seed % GENRES.length] //same gene for the same seed
+  const offset = seed % 100
+  const response = await axios.get(`https://api.deezer.com/search?q=${genre}&limit=1&index=${offset}`)
+  const track = response.data.data[0] //the first .data is axios unwrapping the response, the second .data is the Deezer array, and [0] gets the first (only) track.
+
+  res.json({
+  title: track.title,
+  artist: track.artist.name,
+  cover: track.album.cover_medium,
+  preview: track.preview
+})
+
 })
 
 
@@ -17,3 +47,5 @@ app.get('/api/track', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
